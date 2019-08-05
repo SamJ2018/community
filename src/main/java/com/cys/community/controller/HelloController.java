673b726e128833ca53be267db2f77ad7301bc4a4
SamJ2014 <37620490+SamJ2018@users.noreply.github.com>
2019-08-06 00:21:26 +0800

    init repo

---
+++ src/main/java/com/cys/community/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.cys.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @Author: sam
+ * @create 2019-08-06-12:05 AM
+ * @Description:
+ **/
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
