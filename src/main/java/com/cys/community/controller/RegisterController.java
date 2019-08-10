d8c1b0eda9661ee00d19fb5608fe2ace5c4b0104
Taylor Sam <missbearcn@outlook.com>
2019-08-10 16:46:20 +0800

    improve register

--- src/main/java/com/cys/community/controller/RegisterController.java
+++ src/main/java/com/cys/community/controller/RegisterController.java
@@ -1,8 +1,16 @@
 package com.cys.community.controller;
 
+import com.cys.community.model.User;
+import com.cys.community.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
 
 /**
  * @author sam
@@ -13,9 +21,26 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class RegisterController {
 
+    @Autowired
+    UserService userService;
+
     @GetMapping("/register")
     public String register() {
         return "register";
     }
 
+    @PostMapping("/afterRegister")
+    public String after(@RequestParam(value = "username") String username,
+                        @RequestParam(value = "password") String password,
+                        HttpServletResponse response) {
+
+        User user = new User();
+        user.setToken(password);
+        user.setName(username);
+        user.setAccountId(UUID.randomUUID().toString());
+        user.setAvatarUrl("/images/avatar.png");
+        userService.createOrUpdate(user);
+        response.addCookie(new Cookie("token", password));
+        return "redirect:/";
+    }
 }
