3ce95c084d4de212805489a21bda9b1538b9258d
Taylor Sam <missbearcn@outlook.com>
2019-08-10 17:22:40 +0800

    add tag

--- src/main/java/com/cys/community/cache/TagCache.java
+++ src/main/java/com/cys/community/cache/TagCache.java
@@ -10,14 +10,22 @@ import java.util.stream.Collectors;
 
 /**
  * @author sam
- * @since  2019-08-08-1:35 PM
  * @apiNote :
+ * @since 2019-08-08-1:35 PM
  **/
 public class TagCache {
 
 
     public static List<TagDTO> get() {
+
         ArrayList<TagDTO> tagDTOS = new ArrayList<>();
+
+        TagDTO chatchar = new TagDTO();
+        chatchar.setCategoryName("闲聊");
+        chatchar.setTags(Arrays.asList("情感", "友情", "男朋友", "女朋友", "同性", "交友", "暧昧", "炫富", "杂谈", "衣食住行..."));
+        tagDTOS.add(chatchar);
+
+
         TagDTO program = new TagDTO();
         program.setTags(Arrays.asList("javascript", "php", "css", "html", "html5", "java", "node.js", "python", "c++", "c", "golang", "objective-c", "typescript", "shell ", "c#", "swift", "sass", "bash", "ruby", "less", "asp.net", "lua", "scala", "coffeescript", "actionscript", "rust", "erlang", "perl"));
         program.setCategoryName("开发语言");
@@ -43,6 +51,7 @@ public class TagCache {
         devtools.setTags(Arrays.asList("git", "github", "visual-studio-code", "vim", "sublime-text", "xcode", "intellij-idea", "eclipse", "maven", "ide", "svn", "visual-studio", "atom", "emacs", "textmate", "hg"));
         tagDTOS.add(devtools);
 
+
         return tagDTOS;
     }
 
@@ -50,7 +59,7 @@ public class TagCache {
         String[] split = StringUtils.split(tags, ",");
         List<TagDTO> tagDTOS = get();
         List<String> tagList = tagDTOS.stream().flatMap(tag -> tag.getTags().stream()).collect(Collectors.toList());
-        String invalidList = Arrays.stream(split).filter(t ->StringUtils.isBlank(t) || !tagList.contains(t)).collect(Collectors.joining(","));
+        String invalidList = Arrays.stream(split).filter(t -> StringUtils.isBlank(t) || !tagList.contains(t)).collect(Collectors.joining(","));
         System.out.println(invalidList);
         return invalidList;
     }
